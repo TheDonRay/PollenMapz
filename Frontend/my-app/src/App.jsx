0cfb92c600ca03575d3da615c7ffc6c23a2b7ad0
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-05-29 00:29:29 -0400

    got the skeleton titile set up focused on functionality for now

---
+++ Frontend/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'; // Make sure this line is here to link the CSS
+import './SearchBar'; 
+import SearchBar from './SearchBar';
+
+function App() {
+  return (
+    <div className="center-container">
+      <h1>Welcome To PollenMapz 🌸</h1>
+      <SearchBar />
+    </div> 
+  );
+}
+
+export default App;
---
+++ Frontend/my-app/src/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function SearchBar() { 
+    return(
+        <div className="searchbar"> 
+            <h2>holds the search bar</h2>
+        </div>
+    ); 
+} 
+
+export default SearchBar; 

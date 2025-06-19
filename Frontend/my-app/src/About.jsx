4ba552588620523e14e4587c07e0f3866c208092
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-18 21:30:33 -0400

    font added

--- Frontend/my-app/src/About.jsx
+++ Frontend/my-app/src/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import './styling/aboutPage.css' 
-import { useState, useRef } from 'react'; 
+import { useEffect, useRef } from 'react'; 
 
 
 
@@ -8,7 +8,8 @@ import { useState, useRef } from 'react';
 // do want to include some other 3d features from the library i found 
 
 function About() { 
-    // basically want it to import the backend data but first i need to import the background image 
+    // basically want it to import the backend data but first i need to import the background animation with vantaJS so lets try that out.  
+    // lowkey also want to add a cool font. 
 }
 
 export default About; 

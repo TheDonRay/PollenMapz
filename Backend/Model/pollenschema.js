0f45580d5abf6396ebe457d1d9abcacd35e1f550
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-22 20:42:16 -0400

    pollen schema model added dont know if i will need it tho

---
+++ Backend/Model/pollenschema.js
@@ -0,0 +1,7 @@
+import mongoose  from "mongoose"; 
+
+// pollen schema but i dont know if i will need this since data is just being mapped on ? i still need to add the pollen data to the data folder in the project. 
+const PollenDataSchema = new mongoose.Schema({ 
+
+}); 
+

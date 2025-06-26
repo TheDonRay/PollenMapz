dcd99577375afb782328cd53ab8cc264ef369833
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-25 23:40:51 -0400

    schema for parklocation  has been added

---
+++ Backend/Model/park.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); 
+
+// the reason why im doing this model schema is to determine how the data is  going to be stored in my mongoDb 
+// set up the schema of the data location query that i want to get and understand how to map that data 
+// may need to create another shcema as well for the pollen one 
+
+const parkLocSchema = new mongoose.Schema({  
+    // follow the syntax of what i have 
+    name: String, 
+    address: String,  
+    coordinates: { 
+        lat: Number, // the reason so is because we need to convert the actual address from csv to json data. 
+        lng: Number, 
+    }, 
+    borough: String, 
+    multipolygon: Object, 
+}); 
+
+// export thi sschema as such  
+// remember agani we are doing commonJS 
+
+const ParkLocationModel = mongoose.model('ParkLocation', parkLocSchema); 
+module.exports = ParkLocationModel; 
+

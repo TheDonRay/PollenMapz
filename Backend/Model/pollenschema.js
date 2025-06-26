326d4ffb2c7c601df69edf1d2df027d7df0ee3ed
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-25 23:30:24 -0400

    took out one of the schemas going to start the schema script for the parks

--- Backend/Model/pollenschema.js
+++
@@ -1,7 +0,0 @@
-import mongoose  from "mongoose"; 
-
-// pollen schema but i dont know if i will need this since data is just being mapped on ? i still need to add the pollen data to the data folder in the project. 
-const PollenDataSchema = new mongoose.Schema({ 
-
-}); 
-

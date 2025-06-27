328c07550421fd3c868a89c086bbb73ac13b91de
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-26 22:23:47 -0400

    abt to test out the csv to json connection

--- Backend/scripts/importParkData.js
+++ Backend/scripts/importParkData.js
@@ -6,10 +6,27 @@ const csv = require('csvtojson');
 const ParkData = require('../Model/park.js'); // remember we are using the two dots the .. means "go up one folder" 
 
 
+
 // connect to the db 
 mongoose.connect('mongodb://localhost:27017/pollenmapz', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
 }); 
 
+// now i need to load and parse the csv 
+csv()
+    .fromFile("../data/parkdata.csv") // path to the data  
+    .then(async (jsonArray) => { 
+        const transformingData = jsonArray.map(parkData => ({ // transforming data represents the array of objects 
+            name: parkData.name, 
+            address: parkData.address, 
+            coordinates: { 
+                lat: parseFloat(parkData.lat), 
+                lng: parseFloat(parkData.lng) 
+            }
+        }));  
+
+        // now we want to go ahead and inset into mongodb 
+        await 
+    })
 

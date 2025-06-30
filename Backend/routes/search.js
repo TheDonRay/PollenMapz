c8cd694f21f1fdcdfb4967c295f2757955bb14f6
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-06-29 21:56:55 -0400

    familiar with the querying here

--- Backend/routes/search.js
+++ Backend/routes/search.js
@@ -5,10 +5,16 @@ const Park = require("../Model/park.js"); // include the model of how the databa
 
 // to get the user location 
 
+// now im familiar with the querying here 
 searchLocation.get('/search', async (req, res) => {  // represents the search page for when you are searching for location. 
-    //  need to query database based of user location entered in the search bar  
-    // const query = req.query.location; 
-    // const result = await query
+    // need to be able to get the search req that use rputs in  
+    res.send("Yes this backend is running"); 
+    // can i do a settime out function for this ? 
+    setTimeout(() => { 
+        const SearchedLocation = req.query.SearchedLocation; 
+        console.log(SearchedLocation); 
+    }, 5000);   
+    console.log(`Your location is ${SearchedLocation}`); 
 }); 
 
 module.exports = searchLocation; 

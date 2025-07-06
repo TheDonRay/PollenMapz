ea30717abf84d2b2dedf6548010da9c41d0d2125
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-07-05 20:43:06 -0400

    still trying to figure out this db issue

--- Backend/routes/search.js
+++ Backend/routes/search.js
@@ -1,5 +1,5 @@
 const express = require("express"); 
-const searchLocation = express.Router();  
+const searchLocation = express.Router(); // name of the route.   
 // import the model schema 
 const Park = require("../Model/park.js"); // include the model of how the database should look with the data there. 
 
@@ -8,19 +8,33 @@ const Park = require("../Model/park.js"); // include the model of how the databa
 // need to write this in a try catch case using await. Then console log errors to see where its going wrong. 
 searchLocation.get('/search', async (req, res) => { // simple query to get the user location based of the location they put.  ,
     const getUserLocation = req.query.getUserLocation;   
-    console.log(req.query.getUserLocation); 
+    
+    try {   
+        console.log("The Query reads the user entered this location:", getUserLocation);  
+        console.log("Mongoose is using this collection:", Park.collection.name); 
+
+        if (!getUserLocation){ 
+            return res.status(400).json({ error: "Missing 'getUserLocation' query"})
+        }
+        const result = await Park.find({ name: new RegExp(getUserLocation, 'i')});  
+        
+        res.json(result); 
+    } 
+    catch (error) {     
+        console.error('Server failed', error);  
+        res.status(500).json({ error: "Internal Server Error. "}); // wriritng errors here 
+    }
     
     // here the user result will go where we need to find that data that was posted here but for now I will just console.log the item  
-    // console.log(`You entered: ${getUserLocation}`); 
-    const result = await Park.find({ name: new RegExp(getUserLocation, 'i')});  // we call this line essentially querying the database.    
-    console.log(result); 
+     
+      // we call this line essentially querying the database.    
+    
     // what this line does here is that the Park.find is a mongoose query that searches the MongoDB collection associated with the park model 
     //the line { name: new RegExp(getUserLocation, 'i)} this is the filter criteria. Its saying search for documents where the name field matches the regular expression. 
     // query which in our case is the getUserLocation is expected to be a string containing the user's search input 
     // new RegExp(query (in our case query = getUserLocation), 'i') creates a regular expression that matches the value of the query, and the 'i' flag makes it case-insensitive.  
     // example: if query = central it will match with "Central Park" etc
-    res.json(result); 
-}); 
+});  
 
 module.exports = searchLocation; 
 
